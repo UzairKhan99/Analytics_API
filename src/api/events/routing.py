356f043c758e13bf58346f246089ad8f377e6b7e
UzairKhan99 <uzairuddinkhan2@gmail.com>
2026-04-22 12:23:36 +0500

    First Commit

---
+++ src/api/events/routing.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from typing import Union
+from .schemas import EventSchema
+
+router = APIRouter()
+
+
+@router.get("/")
+def get_items():
+    return {
+        "items": [1, 2, 3]
+    }
+
+
+@router.get("/{event_id}")
+def get_items(event_id: int) -> EventSchema:
+    return {
+        "id": event_id
+    }
