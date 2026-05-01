3d2da90c71d5e9198eac65fe967ee59aece6a703
UzairKhan99 <uzairuddinkhan2@gmail.com>
2026-05-01 17:30:00 +0500

    Move database session module

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,15 +1,17 @@
 from sqlmodel import Field, Session, SQLModel, create_engine
 from api.db.config import DATABASE_URL
-
+import timescaledb
 if not DATABASE_URL:
     raise NotImplementedError("DATABASE URL needs to be set.")
 
-engine = create_engine(DATABASE_URL)
+engine = timescaledb.create_engine(DATABASE_URL, timezone='UTC')
 
 
 def init_db():
     print("creating database")
     SQLModel.metadata.create_all(engine)
+    print("creating hypertables")
+    timescaledb.metadata.create_all(engine)
 
 
 def get_session():
