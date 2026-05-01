9fb948f4a7d0f8cb857ca7446c27bb8e423c0566
UzairKhan99 <uzairuddinkhan2@gmail.com>
2026-05-01 19:25:00 +0500

    Integrate event routes with database session

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from .models import EventModel, EventsListSchema, EventCreateSchema, UpdateSchema
 from .session import get_session
@@ -8,7 +8,7 @@ router = APIRouter()
 
 @router.get("/", response_model=EventsListSchema)
 def read_events(session: Session = Depends(get_session)):
-    query = select(EventModel)
+    query = select(EventModel).order_by(EventModel.id.desc()).limit(10)
     results = session.exec(query).all()
     return {
         "results": results,
@@ -30,10 +30,24 @@ def create_events(payload: EventCreateSchema, session: Session = Depends(get_ses
 def get_event(event_id: int, session: Session = Depends(get_session)):
     query = select(EventModel).where(EventModel.id == event_id)
     result = session.exec(query).first()
+    if result is None:
+        raise HTTPException(status_code=404, detail="Event not found")
     return result
 
 
 @router.put("/{event_id}", response_model=EventModel)
-def update_event(event_id: int, payload: UpdateSchema) -> EventModel:
+def update_event(event_id: int,
+                 payload: UpdateSchema,
+                 session: Session = Depends(get_session)):
+    query = select(EventModel).where(EventModel.id == event_id)
+    event = session.exec(query).first()
+    if event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
     data = payload.model_dump()
-    return {"id": event_id, **data}
+    for keys, value in data.item():
+        setattr(event, keys, value)
+    obj.updated_at = get__utc_now()
+    session.add(event)
+    session.commit()
+    session.refresh(event)
+    return event
